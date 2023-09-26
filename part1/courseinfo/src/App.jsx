bbced5b97a0c87738513d25b28e866364478674a
Emerald Teve <emerald.teve@gmail.com>
2023-09-26 16:25:14 -0500

    part1 courseinfo step1 complete

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,44 @@
+const App = () => {
+  const course = 'Half Stack Application Development'
+  const Header = (props) => {
+    return (
+      <>
+        <h1>{props.course}</h1>
+      </>
+    )
+  }
+
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  const Content = (props) => {
+    return (
+      <>
+        <p>{props.part} {props.exercise}</p>
+      </>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <>
+        <p>Number of exercises {props.total}</p>
+      </>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
