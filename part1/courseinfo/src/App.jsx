5ef0c696a6daa04b508060fd2209e09b5816584f
Emerald Teve <emerald.teve@gmail.com>
2023-09-26 16:30:23 -0500

    Completed part1 courseinfo step2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   const exercises1 = 10
   const exercises2 = 7
   const exercises3 = 14
-  const Content = (props) => {
+  const Part = (props) => {
     return (
       <>
         <p>{props.part} {props.exercise}</p>
@@ -33,9 +33,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
