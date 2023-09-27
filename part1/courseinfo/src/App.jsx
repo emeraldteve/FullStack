1b5b8cbdac781235f5410b00a58987c3e99a6c67
Emerald Teve <emerald.teve@gmail.com>
2023-09-27 09:44:36 -0500

    part1.3 courseinfo step3 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,18 @@
 const App = () => {
-  const course = 'Half Stack Application Development'
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   const Header = (props) => {
     return (
       <>
@@ -8,12 +21,6 @@ const App = () => {
     )
   }
 
-  const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
   const Part = (props) => {
     return (
       <>
@@ -33,10 +40,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Part part={part1.name} exercise={part1.exercises} />
+      <Part part={part2.name} exercise={part2.exercises} />
+      <Part part={part3.name} exercise={part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
